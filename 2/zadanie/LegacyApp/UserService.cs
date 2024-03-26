69b2b1507b569b742cae4f4cdae271e6bfae62bc
Mateusz Kacprzak <matkac98@gmail.com>
2024-03-26 18:25:53 +0100

    Delete if statements in UserService

--- 2/zadanie/LegacyApp/UserService.cs
+++ 2/zadanie/LegacyApp/UserService.cs
@@ -6,24 +6,20 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if ((string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName)) || (!email.Contains("@") && !email.Contains(".")))
+            int age = DateTime.Now.Year - dateOfBirth.Year;
+            if (DateTime.Now.Month < dateOfBirth.Month ||
+                (DateTime.Now.Month == dateOfBirth.Month && DateTime.Now.Day < dateOfBirth.Day))
             {
-                return false;
+                age--;
             }
-            /*
-            if (!email.Contains("@") && !email.Contains("."))
-            {
-                return false;
-            }
-            */
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
 
-            if (age < 21)
+            ;
+
+            if ((string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName)) || (!email.Contains("@") && !email.Contains(".")) || (age<21))
             {
                 return false;
             }
+            
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
--- 2/zadanie/LegacyApp/obj/Debug/net8.0/LegacyApp.AssemblyInfo.cs
+++ 2/zadanie/LegacyApp/obj/Debug/net8.0/LegacyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39cf80459aecbea02d157e4503b0df6997dc21cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94820b7d30269066ed2d7b93ee5a36ad563827dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegacyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
