4b20f0729a9b53ff1a72b54540451cec5b7ab7cd
Mateusz Kacprzak <matkac98@gmail.com>
2024-05-09 20:38:15 +0200

    Create folder for topic 4

---
+++ 4/AnimalApp/Repositories/IAnimalsRepository.cs
@@ -0,0 +1,11 @@
+﻿using AnimalApp.Models;
+namespace AnimalApp.Repositories;
+
+public interface IAnimalsRepository
+{
+    IEnumerable<Animal> GetAnimals();
+    int CreateAnimal(Animal animal);
+    Animal GetAnimal(int idAnimal);
+    int UpdateAnimal(Animal animal);
+    int DeleteAnimal(int idAnimal);
+}
