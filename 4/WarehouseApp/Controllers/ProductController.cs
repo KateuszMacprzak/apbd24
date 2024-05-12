495df866c917102b907f1d3f043e5c0e5feeb836
Mateusz Kacprzak <matkac98@gmail.com>
2024-05-12 10:56:10 +0200

    Add Product controller

--- 4/WarehouseApp/Controllers/ProductController.cs
+++ 4/WarehouseApp/Controllers/ProductController.cs
@@ -1,62 +1,7 @@
-﻿using WarehouseApp.Models;
+using WarehouseApp.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace WarehouseApp.Controllers;
-
-// kontroler warehouse
-[Route("api/warehouses")]
-[ApiController]
-public class WarehousesController: ControllerBase
-{
-    private static readonly List<Warehouse> _warehouses = new()
-    {
-        new Warehouse {IdWarehouse = 1, Name = "Warsaw", Address = "Kwiatowa 12"}
-    };
-    [HttpGet]
-    public IActionResult GetWarehouses()
-    {
-        return Ok(_warehouses.OrderBy(warehouse => warehouse.IdWarehouse).ToList()); 
-    }
-    
-
-    [HttpPost]
-    public IActionResult CreateWarehouse(Warehouse warehouse)
-    {
-        _warehouses.Add(warehouse);
-        return StatusCode(StatusCodes.Status201Created);
-    }
-
-    [HttpPut("{id:int}")]
-    public IActionResult UpdateWarehouse(int id, Warehouse warehouse)
-    {
-        var warehouseToEdit= _warehouses.FirstOrDefault(a => a.IdWarehouse == id);
-
-        if (warehouseToEdit == null)
-        {
-            return NotFound($"Warehouse with id {id} was not found");
-        }
-
-        if (warehouseToEdit.IdWarehouse != warehouse.IdWarehouse)
-        {
-            return BadRequest("Id cannot be changed");
-        };
-        _warehouses.Remove(warehouseToEdit);
-        _warehouses.Add(warehouse);
-        return NoContent();    
-    }
-    
-    [HttpDelete("{id:int}")]
-    public IActionResult DeleteWarehouse(int id, Warehouse warehouse)
-    {
-        var warehouseToDelete= _warehouses.FirstOrDefault(a => a.IdWarehouse == id);
-        if (warehouseToDelete == null)
-        {
-            return NoContent();
-        }
-
-        _warehouses.Remove(warehouseToDelete);
-        return NoContent();
-    }
-}
+public class ProductController
 
 // kontroler product
 [Route("api/products")]
@@ -70,7 +15,7 @@ public class ProductController: ControllerBase
         new Product {IdProduct=2, Name="Acyclovir", Description="", Price=45.0, Area=""},
         new Product {IdProduct=3, Name="Allopurinol", Description="", Price=30.8, Area=""},
     }
-        [HttpGet]
+    [HttpGet]
     public IActionResult GetProducts()
     {
         return Ok(_products.OrderBy(product => product.IdProduct).ToList());
