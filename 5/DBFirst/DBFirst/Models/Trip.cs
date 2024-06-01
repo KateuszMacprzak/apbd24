5b17859a923f0f9be19981e9e0ae4b2ba2ea751c
Mateusz Kacprzak <matkac98@gmail.com>
2024-06-01 13:01:41 +0200

    Add Trip Model

---
+++ 5/DBFirst/DBFirst/Models/Trip.cs
@@ -0,0 +1,9 @@
+﻿namespace DBFirst.Models;
+
+public class Trip
+{
+    public int TripId { get; set; }
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public ICollection<ClientTrip> ClientTrips { get; set; }
+}
