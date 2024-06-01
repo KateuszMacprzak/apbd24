c879a66d07a20352b3f0cb368e89fd9733f5ef78
Mateusz Kacprzak <matkac98@gmail.com>
2024-06-01 18:46:10 +0200

    Prescription Model - fix typo

--- 6/EFCodeFirst/EFCodeFirst/Startup.cs
+++ 6/EFCodeFirst/EFCodeFirst/Startup.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using EFCodeFirst.Models;
 using Microsoft.EntityFrameworkCore;
-using EFCodeFirst.Models;
 
 public class Startup
 {
