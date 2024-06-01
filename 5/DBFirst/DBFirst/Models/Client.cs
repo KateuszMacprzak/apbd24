957bcf0d4cd6309f9143024e7b87d06228fdda94
Mateusz Kacprzak <matkac98@gmail.com>
2024-06-01 12:57:40 +0200

    Add Client Model

---
+++ 5/DBFirst/DBFirst/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace DBFirst.Models;
+
+public class Client
+{
+    public int ClientId { get; set; }
+    public string Pesel { get; set; }
+    public string Name { get; set; }
+    public ICollection<ClientTrip> ClientTrips { get; set; }
+}
